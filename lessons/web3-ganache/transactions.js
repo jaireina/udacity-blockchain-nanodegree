e2691235420d3f2b78659c7a1f255d1f42b97edc
Jair Reina <jair.reina@digitas.com>
2021-11-17 23:19:03 -0600

    adding script to execute transactions within ganache

--- lessons/web3-ganache/transactions.js
+++ lessons/web3-ganache/transactions.js
@@ -1,31 +1,32 @@
 const Web3 = require('web3');
-const EthereumTransaction = require('ethereumjs-tx').Transaction;
+const EthereumTransaction = require('ethereumjs-tx');
 const web3 = new Web3('http://127.0.0.1:7545');
 
 const sending = {
-  address: "0x9813CEA533B147DC792AaEEE9CafC0dA86AE4F71",
-  key: Buffer.from("86ff4ca54c30230a3a10163b071f2f0f692c8957a058db0bfc4933e4709b9cdf", 'utf8').toString('hex'),
+  address: "0x76Ba2F248a31e34f6035F8e0889f4bFbE79a6C87",
+  key: Buffer.from("9a21b417331903d1fd04010503cc217453bd01e6d623fc0e22c1040606b512d8", 'hex'),
 }
 
 const receiving = {
-  address: "0x2759641a39802ecC05ebaaE64BeCbE8f0EA1C0A6",
-  key: Buffer.from("9e2a9258cba2c8abecc7a9af0195b5e926e7100d9513cdd397648e3e1f1f73eb", 'utf8').toString('hex')
+  address: "0x498540C4081e27f6c7cb9a68C9A6911Ee1dF8EEb",
+  key: Buffer.from("1536da263caefd5279655e0cc3aead97ba58da95cbe0e676865f76c5b27c6b47", 'hex')
 }
 
 async function printBalance(acc) {
-  console.log(await web3.eth.getBalance(acc));
+  console.log("balance", await web3.eth.getBalance(acc));
 }
 
 // get all acccounts and their balances
 (async function() {
   await printBalance(sending.address);
   await printBalance(receiving.address);
-  console.log('hex', Buffer.from('mierda', 'hex').toString('hex'));
-  console.log(Buffer.from("9e2a9258cba2c8abecc7a9af0195b5e926e7100d9513cdd397648e3e1f1f73eb", 'utf8').toString('hex'))
+
+  const nonce = await web3.eth.getTransactionCount(sending.address);
+
   // Create transaction
   const rawTransaction = {
-    nonce: 0,
-    to: "0x2759641a39802ecC05ebaaE64BeCbE8f0EA1C0A6",
+    nonce,
+    to: receiving.address,
     gasPrice: 20000000,
     gasLimit: 30000,
     value: 100,
@@ -34,13 +35,13 @@ async function printBalance(acc) {
 
   // Sign the transaction
   const transaction = new EthereumTransaction(rawTransaction);
-  // transaction.sign(sending.key);
+  transaction.sign(sending.key);
 
   // // Send the serialized transaction
-  // const serializedTransaction = transaction.serialize();
-  // await web3.eth.sendSignedTransaction(serializedTransaction);
+  const serializedTransaction = transaction.serialize();
+  await web3.eth.sendSignedTransaction(serializedTransaction);
 
-  // await printBalance(sending.address);
-  // await printBalance(receiving.address);
+  await printBalance(sending.address);
+  await printBalance(receiving.address);
 })();
 
